92b34652433bcdbdcc2d13759d857f289b5c444d
Romez <Romez1990@gmail.com>
2019-12-10 21:37:43 +0300

    Make price many-to-many relationship easier to handle in admin

--- services/admin.py
+++ services/admin.py
@@ -1,9 +1,32 @@
-from django.contrib.admin import site
+from django.contrib.admin import ModelAdmin, TabularInline, site
 
 from .models import ServiceGroup, Service, Saloon, Price, Appointment
 
+
+class PricesInLine(TabularInline):
+    model = Price
+    extra = 0
+
+
+class ServiceAdmin(ModelAdmin):
+    inlines = [
+        PricesInLine,
+    ]
+
+    def _prices(self, obj):
+        return obj.prices.all().count()
+
+
+class SaloonAdmin(ModelAdmin):
+    inlines = [
+        PricesInLine,
+    ]
+
+    def _prices(self, obj):
+        return obj.prices.all().count()
+
+
 site.register(ServiceGroup)
-site.register(Service)
-site.register(Saloon)
-site.register(Price)
+site.register(Service, SaloonAdmin)
+site.register(Saloon, SaloonAdmin)
 site.register(Appointment)
